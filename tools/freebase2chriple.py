41bcbc689ed3dca6d2d083ddb323400d20436305
Brian Guarraci <brian@crickethealth.com>
2016-05-23 22:09:51 -0700

    wip

--- tools/freebase2chriple.py
+++ tools/freebase2chriple.py
@@ -4,51 +4,50 @@
 #
 import sqlite3
 import gzip
+import sys
 
 nounsDBConnection = sqlite3.connect("nouns.db")
 nounsDBConnection.text_factory = str
 nounsDB = nounsDBConnection.cursor()
 
-lastNounKey = ""
-lastNounValue = 0
-
 predicatesDBConnection = sqlite3.connect("predicates.db")
 predicatesDBConnection.text_factory = str
 predicatesDB = predicatesDBConnection.cursor()
 
-lastPredicateKey = ""
-lastPredicateValue = 0
+lastKey = ["","",""]
+lastValue = [0,0,0]
 
-def idForNoun(noun):
-    global lastNounKey
-    global lastNounValue
-    if noun == lastNounKey:
-        return lastNounValue
+def idForNoun(noun,pos):
+    global lastKey
+    global lastValue
+    if noun == lastKey[pos]:
+        return lastValue[pos]
     a = nounsDB.execute("SELECT id FROM nouns WHERE noun = ?", (noun, ))
     r = a.fetchall()
-    lastNounKey = noun
-    lastNounValue = r[0][0] if len(r) > 0 else 0
-    return lastNounValue
+    lastKey[pos] = noun
+    lastValue[pos] = r[0][0] if len(r) > 0 else 0
+    return lastValue[pos]
 
-def idForPredicate(predicate):
-    global lastPredicateKey
-    global lastPredicateValue
-    if predicate == lastPredicateKey:
-        return lastPredicateValue
+def idForPredicate(predicate, pos):
+    global lastKey
+    global lastValue
+    if predicate == lastKey[pos]:
+        return lastValue[pos]
     a = predicatesDB.execute("SELECT id FROM predicates WHERE predicate = ?", (predicate, ))
     r = a.fetchall()
-    lastPredicateKey = predicate
-    lastPredicateValue = r[0][0] if len(r) > 0 else 0
-    return lastPredicateValue
+    lastKey[pos] = predicate
+    lastValue[pos] = r[0][0] if len(r) > 0 else 0
+    return lastValue[pos]
 
-with gzip.open("freebase-rdf-latest.gz", 'r') as fin:
-    for line in fin:
-        line = line.strip()
-        (subject, predicate, obj, junk) = line.split('\t')
-        subjectId = idForNoun(subject)
-        predicateId = idForPredicate(predicate)
-        objId = idForNoun(obj)
-        print "{} {} {}".format(subjectId, predicateId, objId)
+with gzip.open(sys.argv[1]+".out.gz",'w') as fout:
+    with gzip.open(sys.argv[1], 'r') as fin:
+        for line in fin:
+            line = line.strip()
+            (subject, predicate, obj, junk) = line.split('\t')
+            subjectId = idForNoun(subject,0)
+            predicateId = idForPredicate(predicate,1)
+            objId = idForNoun(obj,2)
+            fout.write("{} {} {}\n".format(subjectId, predicateId, objId))
 
 nounsDBConnection.close()
 predicatesDBConnection.close()
